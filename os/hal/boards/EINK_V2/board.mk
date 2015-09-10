c242234abc7a46fcb5ee231a2d9a219574f91037
Kenneth Lim <kennethlimcp@gmail.com>
2015-09-10 11:13:36 +0800

    missed makefile changes for board
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- os/hal/boards/EINK_V2/board.mk
+++ os/hal/boards/EINK_V2/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/os/hal/boards/ST_NUCLEO_F103RB/board.c
+BOARDSRC = ${CHIBIOS}/os/hal/boards/EINK_V2/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/os/hal/boards/ST_NUCLEO_F103RB
+BOARDINC = ${CHIBIOS}/os/hal/boards/EINK_V2
