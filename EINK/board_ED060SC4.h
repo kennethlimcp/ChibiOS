9425f0749059087f5bab20b58b0958c1500a632b
Kenneth Lim <kennethlimcp@gmail.com>
2016-02-04 21:12:06 +0800

    working compile with ugfx library added
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

---
+++ EINK/board_ED060SC4.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2013 Petteri Aimonen <jpa@gfx.mail.kapsi.fi>
+ * This file is released to the public domain.
+ *
+ */
+
+/* Board interface definitions for ED060SC4 PrimeView E-ink panel.
+ *
+ * You should implement the following functions to define the interface to
+ * the panel on your board.
+ */
+
+#ifndef _GDISP_LLD_BOARD_H
+#define _GDISP_LLD_BOARD_H
+
+/* Set up IO pins for the panel connection. */
+static inline void init_board(GDisplay *g) {
+
+}
+
+/* Delay for display waveforms. Should be an accurate microsecond delay. */
+static void eink_delay(int us)
+{
+	//halPolledDelay(US2RTT(us));
+	chSysPolledDelayX(US2ST(us));
+}
+
+/* Turn the E-ink panel Vdd supply (+3.3V) on or off. */
+static inline void setpower_vdd(GDisplay *g, bool_t on) {
+    if (on)
+    {
+					palWritePad(GPIOB, GPIOB_EINK_VDD, on);
+					palWritePad(GPIOC, GPIOC_SMPS_ON, !on);
+					palWritePad(GPIOC, GPIOC_POS_ON, !on);
+    //     chSysLock();
+    //     palSetPadMode(GPIOA, GPIOA_EINK_VDD, PAL_MODE_OUTPUT_PUSHPULL);
+    //     chSysUnlock();
+    //     palWritePad(GPIOA, GPIOA_EINK_VDD, on);
+    }
+    else
+    {
+	    // Set pin as input to avoid short-circuits in case some IO pin is high.
+	    chSysLock();
+	    palSetPadMode(GPIOB, GPIOB_EINK_VDD, PAL_MODE_INPUT_PULLDOWN);
+	    chSysUnlock();
+	    // palWritePad(GPIOA, GPIOA_EINK_VDD, on);
+					palWritePad(GPIOB, GPIOB_EINK_VDD, on);
+					palWritePad(GPIOC, GPIOC_SMPS_ON, !on);
+					palWritePad(GPIOC, GPIOC_POS_ON, !on);
+    }
+}
+
+/* Turn the E-ink panel negative supplies (-15V, -20V) on or off. */
+static inline void setpower_vneg(GDisplay *g, bool_t on) {
+    //palWritePad(GPIOB, GPIOB_EINK_PWR, !on);
+				// palWritePad(GPIOC, GPIOC_NEG_EN, on);
+    // // Some hackery around the fact that GPIOB0 is not really 5V-tolerant..
+    // chSysLock();
+    // if (on)
+    //     palSetPadMode(GPIOB, GPIOB_EINK_PWR, PAL_MODE_OUTPUT_OPENDRAIN);
+    // else
+    //     palSetPadMode(GPIOB, GPIOB_EINK_PWR, PAL_MODE_INPUT);
+    // chSysUnlock();
+
+	//palWritePad(GPIOA, GPIOA_VNEG_CTRL, on);
+	palWritePad(GPIOC, GPIOC_NEG_EN, on);
+}
+
+/* Turn the E-ink panel positive supplies (-15V, -20V) on or off. */
+static inline void setpower_vpos(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOA, GPIOA_VPOS_CTRL, on);
+	palWritePad(GPIOC, GPIOC_POS_EN, on);
+}
+
+/* Set the state of the LE (source driver Latch Enable) pin. */
+static inline void setpin_le(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_LE, on);
+	palWritePad(GPIOC, GPIOC_EINK_LE, on);
+}
+
+/* Set the state of the OE (source driver Output Enable) pin. */
+static inline void setpin_oe(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_OE, on);
+	palWritePad(GPIOC, GPIOC_EINK_OE, on);
+}
+
+/* Set the state of the CL (source driver Clock) pin. */
+static inline void setpin_cl(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_CL, on);
+	 palWritePad(GPIOC, GPIOC_EINK_CL, on);
+}
+
+/* Set the state of the SPH (source driver Start Pulse Horizontal) pin. */
+static inline void setpin_sph(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_SPH, on);
+	palWritePad(GPIOC, GPIOC_EINK_SPH, on);
+}
+
+/* Set the state of the D0-D7 (source driver Data) pins. */
+static inline void setpins_data(GDisplay *g, uint8_t value) {
+	//palWriteGroup(GPIOB, 0xFF, GPIOB_EINK_D0, value);
+	palWriteGroup(GPIOA, 0xFF, GPIOA_EINK_D0, value);
+}
+
+/* Set the state of the CKV (gate driver Clock Vertical) pin. */
+static inline void setpin_ckv(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_CKV, on);
+	palWritePad(GPIOC, GPIOC_EINK_CKV, on);
+}
+
+/* Set the state of the GMODE (gate driver Gate Mode) pin. */
+static inline void setpin_gmode(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOC, GPIOC_EINK_GMODE, on);
+	palWritePad(GPIOC, GPIOC_EINK_GMODE, on);
+}
+
+/* Set the state of the SPV (gate driver Start Pulse Vertical) pin. */
+static inline void setpin_spv(GDisplay *g, bool_t on) {
+	//palWritePad(GPIOB, GPIOB_EINK_SPV, on);
+	palWritePad(GPIOC, GPIOC_EINK_SPV, on);
+}
+
+#endif
--- EINK/main.c
+++ EINK/main.c
@@ -16,247 +16,249 @@
 
 #include "ch.h"
 #include "hal.h"
-#include "eink.h"
+#include <gfx.h>
 
-#ifndef GDISP_SCREEN_HEIGHT
-#       define GDISP_SCREEN_HEIGHT 600
-#endif
+//#include "eink.h"
 
-#ifndef GDISP_SCREEN_WIDTH
-#       define GDISP_SCREEN_WIDTH 800
-#endif
+// #ifndef GDISP_SCREEN_HEIGHT
+// #       define GDISP_SCREEN_HEIGHT 600
+// #endif
+//
+// #ifndef GDISP_SCREEN_WIDTH
+// #       define GDISP_SCREEN_WIDTH 800
+// #endif
+//
+//
+// /** Fast vertical clock pulse for gate driver, used during initializations */
+// static void vclock_quick(void)
+// {
+// 		setpin_ckv(TRUE);
+// 		eink_delay(1);
+// 		setpin_ckv(FALSE);
+// 		eink_delay(4);
+// }
+//
+// /** Horizontal clock pulse for clocking data into source driver */
+// static void hclock(void)
+// {
+//     clockdelay();
+//     setpin_cl(TRUE);
+//     clockdelay();
+//     setpin_cl(FALSE);
+// }
+//
+// /** Start a new vertical gate driver scan from top.
+//  * Note: Does not clear any previous bits in the shift register,
+//  *       so you should always scan through the whole display before
+//  *       starting a new scan.
+//  */
+// static void vscan_start(void)
+// {
+//     setpin_gmode(TRUE);
+//     vclock_quick();
+//     setpin_spv(FALSE);
+// 				vclock_quick();
+//     setpin_spv(TRUE);
+//     vclock_quick();
+// }
+//
+// /** Waveform for strobing a row of data onto the display.
+//  * Attempts to minimize the leaking of color to other rows by having
+//  * a long idle period after a medium-length strobe period.
+//  */
+// static void vscan_write(void)
+// {
+//     setpin_ckv(TRUE);
+//     setpin_oe(TRUE);
+//     eink_delay(5);
+//     setpin_oe(FALSE);
+//     setpin_ckv(FALSE);
+//     eink_delay(200);
+// }
+//
+// /** Waveform used when clearing the display. Strobes a row of data to the
+//  * screen, but does not mind some of it leaking to other rows.
+//  */
+// static void vscan_bulkwrite(void)
+// {
+//     setpin_ckv(TRUE);
+//     eink_delay(20);
+//     setpin_ckv(FALSE);
+//     eink_delay(200);
+// }
+//
+// /** Waveform for skipping a vertical row without writing anything.
+//  * Attempts to minimize the amount of change in any row.
+//  */
+// static void vscan_skip(void)
+// {
+//     setpin_ckv(TRUE);
+//     eink_delay(1);
+//     setpin_ckv(FALSE);
+//     eink_delay(100);
+// }
+//
+// /** Stop the vertical scan. The significance of this escapes me, but it seems
+//  * necessary or the next vertical scan may be corrupted.
+//  */
+// static void vscan_stop(void)
+// {
+//     setpin_gmode(FALSE);
+//     vclock_quick();
+//     vclock_quick();
+//     vclock_quick();
+//     vclock_quick();
+//     vclock_quick();
+// }
+//
+// /** Start updating the source driver data (from left to right). */
+// static void hscan_start(void)
+// {
+//     /* Disable latching and output enable while we are modifying the row. */
+//     setpin_le(FALSE);
+//     setpin_oe(FALSE);
+//
+//     /* The start pulse should remain low for the duration of the row. */
+//     setpin_sph(FALSE);
+// }
+//
+// /** Write data to the horizontal row. */
+// static void hscan_write(const uint8_t *data, int count)
+// {
+//     while (count--)
+//     {
+//         /* Set the next byte on the data pins */
+//         setpins_data(*data++);
+//
+//         /* Give a clock pulse to the shift register */
+//         hclock();
+//     }
+// }
+//
+// /** Finish and transfer the row to the source drivers.
+//  * Does not set the output enable, so the drivers are not yet active. */
+// static void hscan_stop(void)
+// {
+//     /* End the scan */
+//     setpin_sph(TRUE);
+//     hclock();
+//
+//     /* Latch the new data */
+//     setpin_le(TRUE);
+//     clockdelay();
+//     setpin_le(FALSE);
+// }
+//
+// /** Turn on the power to the E-Ink panel, observing proper power sequencing. */
+// static void power_on(void)
+// {
+//     unsigned i;
+//
+//     /* First the digital power supply and signal levels. */
+//     setpower_vdd(TRUE);
+//     setpin_le(FALSE);
+//     setpin_oe(FALSE);
+//     setpin_cl(FALSE);
+//     setpin_sph(TRUE);
+//     setpins_data(0);
+//     setpin_ckv(FALSE);
+//     setpin_gmode(FALSE);
+//     setpin_spv(TRUE);
+//
+//     /* Min. 100 microsecond delay after digital supply */
+//     eink_delay(100);
+//
+//     /* Then negative voltages and min. 1000 microsecond delay. */
+//     setpower_vneg(TRUE);
+//     eink_delay(1000);
+//
+//     /* Finally the positive voltages. */
+//     setpower_vpos(TRUE);
+//
+//     /* Clear the vscan shift register */
+//     vscan_start();
+//     for (i = 0; i < GDISP_SCREEN_HEIGHT; i++)
+//         vclock_quick();
+//     vscan_stop();
+// }
+//
+// /** Turn off the power, observing proper power sequencing. */
+// static void power_off()
+// {
+//     /* First the high voltages */
+//     setpower_vpos(FALSE);
+//     setpower_vneg(FALSE);
+//
+//     /* Wait for any capacitors to drain */
+//     chThdSleepMilliseconds(100);
+//
+//     /* Then put all signals and digital supply to ground. */
+//     setpin_le(FALSE);
+//     setpin_oe(FALSE);
+//     setpin_cl(FALSE);
+//     setpin_sph(FALSE);
+//     setpins_data(0);
+//     setpin_ckv(FALSE);
+//     setpin_gmode(FALSE);
+//     setpin_spv(FALSE);
+//     setpower_vdd(FALSE);
+// }
+//
+//
+// #define BYTE_WHITE 0xAA
+// #define BYTE_BLACK 0x55
+//
+// static void subclear(void)
+// {
+//     unsigned x, y;
+//     uint8_t byte;
+//
+//     hscan_start();
+//     //byte = color ? BYTE_WHITE : BYTE_BLACK;
+// 				byte = BYTE_BLACK;
+//     for (x = 0; x < GDISP_SCREEN_WIDTH; x++)
+//     {
+//         hscan_write(&byte, 1);
+//     }
+//     hscan_stop();
+//
+//     setpin_oe(TRUE);
+//     vscan_start();
+//     for (y = 0; y < GDISP_SCREEN_HEIGHT; y++)
+//     {
+//         vscan_bulkwrite();
+//     }
+//     vscan_stop();
+//     setpin_oe(FALSE);
+// }
+//
 
-
-/** Fast vertical clock pulse for gate driver, used during initializations */
-static void vclock_quick(void)
-{
-		setpin_ckv(TRUE);
-		eink_delay(1);
-		setpin_ckv(FALSE);
-		eink_delay(4);
-}
-
-/** Horizontal clock pulse for clocking data into source driver */
-static void hclock(void)
-{
-    clockdelay();
-    setpin_cl(TRUE);
-    clockdelay();
-    setpin_cl(FALSE);
-}
-
-/** Start a new vertical gate driver scan from top.
- * Note: Does not clear any previous bits in the shift register,
- *       so you should always scan through the whole display before
- *       starting a new scan.
- */
-static void vscan_start(void)
-{
-    setpin_gmode(TRUE);
-    vclock_quick();
-    setpin_spv(FALSE);
-				vclock_quick();
-    setpin_spv(TRUE);
-    vclock_quick();
-}
-
-/** Waveform for strobing a row of data onto the display.
- * Attempts to minimize the leaking of color to other rows by having
- * a long idle period after a medium-length strobe period.
- */
-static void vscan_write(void)
-{
-    setpin_ckv(TRUE);
-    setpin_oe(TRUE);
-    eink_delay(5);
-    setpin_oe(FALSE);
-    setpin_ckv(FALSE);
-    eink_delay(200);
-}
-
-/** Waveform used when clearing the display. Strobes a row of data to the
- * screen, but does not mind some of it leaking to other rows.
- */
-static void vscan_bulkwrite(void)
-{
-    setpin_ckv(TRUE);
-    eink_delay(20);
-    setpin_ckv(FALSE);
-    eink_delay(200);
-}
-
-/** Waveform for skipping a vertical row without writing anything.
- * Attempts to minimize the amount of change in any row.
- */
-static void vscan_skip(void)
-{
-    setpin_ckv(TRUE);
-    eink_delay(1);
-    setpin_ckv(FALSE);
-    eink_delay(100);
-}
-
-/** Stop the vertical scan. The significance of this escapes me, but it seems
- * necessary or the next vertical scan may be corrupted.
- */
-static void vscan_stop(void)
-{
-    setpin_gmode(FALSE);
-    vclock_quick();
-    vclock_quick();
-    vclock_quick();
-    vclock_quick();
-    vclock_quick();
-}
-
-/** Start updating the source driver data (from left to right). */
-static void hscan_start(void)
-{
-    /* Disable latching and output enable while we are modifying the row. */
-    setpin_le(FALSE);
-    setpin_oe(FALSE);
-
-    /* The start pulse should remain low for the duration of the row. */
-    setpin_sph(FALSE);
-}
-
-/** Write data to the horizontal row. */
-static void hscan_write(const uint8_t *data, int count)
-{
-    while (count--)
-    {
-        /* Set the next byte on the data pins */
-        setpins_data(*data++);
-
-        /* Give a clock pulse to the shift register */
-        hclock();
-    }
-}
-
-/** Finish and transfer the row to the source drivers.
- * Does not set the output enable, so the drivers are not yet active. */
-static void hscan_stop(void)
-{
-    /* End the scan */
-    setpin_sph(TRUE);
-    hclock();
-
-    /* Latch the new data */
-    setpin_le(TRUE);
-    clockdelay();
-    setpin_le(FALSE);
-}
-
-/** Turn on the power to the E-Ink panel, observing proper power sequencing. */
-static void power_on(void)
-{
-    unsigned i;
-
-    /* First the digital power supply and signal levels. */
-    setpower_vdd(TRUE);
-    setpin_le(FALSE);
-    setpin_oe(FALSE);
-    setpin_cl(FALSE);
-    setpin_sph(TRUE);
-    setpins_data(0);
-    setpin_ckv(FALSE);
-    setpin_gmode(FALSE);
-    setpin_spv(TRUE);
-
-    /* Min. 100 microsecond delay after digital supply */
-    eink_delay(100);
-
-    /* Then negative voltages and min. 1000 microsecond delay. */
-    setpower_vneg(TRUE);
-    eink_delay(1000);
-
-    /* Finally the positive voltages. */
-    setpower_vpos(TRUE);
-
-    /* Clear the vscan shift register */
-    vscan_start();
-    for (i = 0; i < GDISP_SCREEN_HEIGHT; i++)
-        vclock_quick();
-    vscan_stop();
-}
-
-/** Turn off the power, observing proper power sequencing. */
-static void power_off()
-{
-    /* First the high voltages */
-    setpower_vpos(FALSE);
-    setpower_vneg(FALSE);
-
-    /* Wait for any capacitors to drain */
-    chThdSleepMilliseconds(100);
-
-    /* Then put all signals and digital supply to ground. */
-    setpin_le(FALSE);
-    setpin_oe(FALSE);
-    setpin_cl(FALSE);
-    setpin_sph(FALSE);
-    setpins_data(0);
-    setpin_ckv(FALSE);
-    setpin_gmode(FALSE);
-    setpin_spv(FALSE);
-    setpower_vdd(FALSE);
-}
-
-
-#define BYTE_WHITE 0xAA
-#define BYTE_BLACK 0x55
-
-static void subclear(void)
-{
-    unsigned x, y;
-    uint8_t byte;
-
-    hscan_start();
-    //byte = color ? BYTE_WHITE : BYTE_BLACK;
-				byte = BYTE_BLACK;
-    for (x = 0; x < GDISP_SCREEN_WIDTH; x++)
-    {
-        hscan_write(&byte, 1);
-    }
-    hscan_stop();
-
-    setpin_oe(TRUE);
-    vscan_start();
-    for (y = 0; y < GDISP_SCREEN_HEIGHT; y++)
-    {
-        vscan_bulkwrite();
-    }
-    vscan_stop();
-    setpin_oe(FALSE);
-}
-
-
-void gate_waveform(void){
-		unsigned a = 0;
-
-		chSysLockFromISR();
-		setpin_gmode(true);
-		eink_delay(3);
-
-		setpin_spv(false);
-		eink_delay(3);
-
-		vclock_quick();
-		eink_delay(3);
-
-		setpin_spv(true);
-		eink_delay(3);
-
-		for (a = 0; a < GDISP_SCREEN_HEIGHT; a++)
-							vscan_bulkwrite();
-
-		setpin_gmode(false);
-		eink_delay(3);
-
-		vclock_quick();
-		chSysUnlockFromISR();
-		chThdSleepMilliseconds(1);
-}
+// void gate_waveform(void){
+// 		unsigned a = 0;
+//
+// 		chSysLockFromISR();
+// 		setpin_gmode(true);
+// 		eink_delay(3);
+//
+// 		setpin_spv(false);
+// 		eink_delay(3);
+//
+// 		vclock_quick();
+// 		eink_delay(3);
+//
+// 		setpin_spv(true);
+// 		eink_delay(3);
+//
+// 		for (a = 0; a < GDISP_SCREEN_HEIGHT; a++)
+// 							vscan_bulkwrite();
+//
+// 		setpin_gmode(false);
+// 		eink_delay(3);
+//
+// 		vclock_quick();
+// 		chSysUnlockFromISR();
+// 		chThdSleepMilliseconds(1);
+// }
 
 /*
  * Red LED blinker thread, times are in milliseconds.
@@ -267,9 +269,9 @@ static THD_FUNCTION(Thread1, arg) {
 
 		chRegSetThreadName("data");
   while (true) {
-			//chThdSleepMilliseconds(1);
-			gate_waveform();
-			subclear();
+			chThdSleepMilliseconds(1);
+			// gate_waveform();
+			// subclear();
   }
 }
 
@@ -312,8 +314,13 @@ int main(void) {
   chSysInit();
 
 		// e-ink related
-		power_on();
-		setpins_data(0xFF);
+		//power_on();
+		//setpins_data(0xFF);
+		gfxInit();
+
+
+	
+
 
   /*
    * Activates the serial driver 3 using the driver default configuration.
