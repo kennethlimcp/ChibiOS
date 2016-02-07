17dd5f670e0578b59942ff8ca42cdd9ec738944c
Kenneth Lim <kennethlimcp@gmail.com>
2016-02-07 21:45:06 +0800

    feature: use new linker-based cmd shell
    - shell functions now reside in its own .h/.c
    
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- EINK/eink.h
+++ EINK/eink.h
@@ -1,108 +1,11 @@
-#ifndef _EINK_H
-#define _EINK_H
+#ifndef __EINK_H__
+#define __EINK_H__
 
-#include <hal.h>
+#define EINK_OS_VERSION_MAJOR      0
+#define EINK_OS_VERSION_MINOR      1
 
-#define EINK_CLOCKDELAY 4
-#ifndef EINK_CLOCKDELAY
-#       define EINK_CLOCKDELAY 0
-#endif
+#define serialDriver                  (&SD3)
+#define stream_driver                 ((BaseSequentialStream *)serialDriver)
+extern void *stream;
 
-/** Delay between signal changes, to give time for IO pins to change state. */
-static inline void clockdelay(void)
-{
-#if EINK_CLOCKDELAY & 1
-    asm("nop");
-#endif
-#if EINK_CLOCKDELAY & 2
-    asm("nop");
-    asm("nop");
-#endif
-#if EINK_CLOCKDELAY & 4
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-#endif
-#if EINK_CLOCKDELAY & 8
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-    asm("nop");
-#endif
-}
-
-static inline void setpower_vdd(bool on) {
-		palWritePad(GPIOB, GPIOB_EINK_VDD, on);
-		palWritePad(GPIOC, GPIOC_SMPS_ON, !on);
-		palWritePad(GPIOC, GPIOC_POS_ON, !on);
-}
-
-/* Delay for display waveforms. Should be an accurate microsecond delay. */
-static void eink_delay(int us)
-{
-    //chThdSleepMicroseconds(us);
-				// int a;
-				// for(a = 0; a < us;a++)
-				// 	clockdelay();
-				chSysPolledDelayX(US2ST(us));
-}
-
-
-/* Turn the E-ink panel negative supplies (-15V, -20V) on or off. */
-static inline void setpower_vneg(bool on) {
-		palWritePad(GPIOC, GPIOC_NEG_EN, on);
-}
-
-/* Turn the E-ink panel positive supplies (-15V, -20V) on or off. */
-static inline void setpower_vpos(bool on) {
-		palWritePad(GPIOC, GPIOC_POS_EN, on);
-}
-
-/* Set the state of the LE (source driver Latch Enable) pin. */
-static inline void setpin_le(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_LE, on);
-}
-
-/* Set the state of the OE (source driver Output Enable) pin. */
-static inline void setpin_oe(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_OE, on);
-}
-
-/* Set the state of the CL (source driver Clock) pin. */
-static inline void setpin_cl(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_CL, on);
-}
-
-/* Set the state of the SPH (source driver Start Pulse Horizontal) pin. */
-static inline void setpin_sph(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_SPH, on);
-}
-
-/* Set the state of the D0-D7 (source driver Data) pins. */
-static inline void setpins_data(uint8_t value) {
-    palWriteGroup(GPIOA, 0xFF, GPIOA_EINK_D0, value);
-}
-
-/* Set the state of the CKV (gate driver Clock Vertical) pin. */
-static inline void setpin_ckv(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_CKV, on);
-}
-
-/* Set the state of the GMODE (gate driver Gate Mode) pin. */
-static inline void setpin_gmode(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_GMODE, on);
-}
-
-/* Set the state of the SPV (gate driver Start Pulse Vertical) pin. */
-static inline void setpin_spv(bool on) {
-    palWritePad(GPIOC, GPIOC_EINK_SPV, on);
-}
-
-
-
-#endif
+#endif /* __EINK_H__ */
--- EINK/main.c
+++ EINK/main.c
@@ -20,65 +20,6 @@
 #include "chprintf.h"
 #include "eink-shell.h"
 
-
-#define serialDriver                  (&SD3)
-#define stream_driver                 ((BaseSequentialStream *)serialDriver)
-
-/*===========================================================================*/
-/* Command line related.                                                     */
-/*===========================================================================*/
-
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[])
-{
-  size_t n, size;
-
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: mem\r\n");
-    return;
-  }
-  n = chHeapStatus(NULL, &size);
-  chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
-  chprintf(chp, "heap fragments   : %u\r\n", n);
-  chprintf(chp, "heap free total  : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
-{
-  static const char *states[] = {CH_STATE_NAMES};
-  thread_t *tp;
-
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: threads\r\n");
-    return;
-  }
-  chprintf(chp, " addr     pc       stack    prio refs state         name\r\n");
-  tp = chRegFirstThread();
-  do {
-    chprintf(chp, " %-8lx %-8lx %-8lx %4lu %4lu %-12s  %-10s\r\n",
-      (uint32_t)tp, (uint32_t)tp->p_ctx.r13->lr, (uint32_t)tp->p_ctx.r13,
-      (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-      states[tp->p_state],
-      tp->p_name);
-    tp = chRegNextThread(tp);
-  } while (tp != NULL);
-}
-
-static const ShellCommand commands[] = {
-  {"mem", cmd_mem},
-  {"threads", cmd_threads},
-  {NULL, NULL}
-};
-
-static const ShellConfig shellConfig = {
-  (BaseSequentialStream *)serialDriver,
-  commands
-};
-
-static thread_t *shelltp = NULL;
-static THD_WORKING_AREA(waShellThread, 768);
-
 /*===========================================================================*/
 /* Main and generic code.                                                    */
 /*===========================================================================*/
@@ -115,17 +56,10 @@ int main(void) {
   chSysInit();
 
   /*
-   * Activates the serial driver 1 and SDC driver 1 using default
-   * configuration.
-   */
-  sdStart(serialDriver, NULL);
-  //sdcStart(&SDCD1, NULL);
-
-  /*
    * Shell manager initialization.
    */
-  shellInit();
-
+		einkShellInit();
+		einkShellRestart();
   /*
    * Creates the blinker thread.
    */
@@ -135,12 +69,7 @@ int main(void) {
    * Normal main() thread activity, in this demo it does nothing except
    * sleeping in a loop and listen for events.
    */
-  while (TRUE) {
-    if (!shelltp)
-      shelltp = shellCreateStatic(&shellConfig, waShellThread, sizeof(waShellThread), NORMALPRIO);
-    else if (chThdTerminatedX(shelltp)) {
-      chThdRelease(shelltp);    /* Recovers memory of the previous shell.   */
-      shelltp = NULL;           /* Triggers spawning of a new shell.        */
-    }
-  }
+			while(TRUE){
+				chThdSleepMilliseconds(500);
+}
 }
