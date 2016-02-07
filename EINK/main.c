281762cc1949ca96d21c43ae3bdac602dd8a4f11
Kenneth Lim <kennethlimcp@gmail.com>
2016-02-07 20:28:33 +0800

    main: working shell example
    - compilable but not tested with actual hardware yet
    - referenced: https://github.com/sutajiokousagi/chibios-orchard/blob/orchard-r2/orchard/orchard-shell.c
    
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- EINK/main.c
+++ EINK/main.c
@@ -16,9 +16,92 @@
 
 #include "ch.h"
 #include "hal.h"
-#include "display.h"
-#include <gfx.h>
+#include "shell.h"
+#include "chprintf.h"
 
+
+#define serialDriver                  (&SD3)
+#define stream_driver                 ((BaseSequentialStream *)serialDriver)
+
+/*===========================================================================*/
+/* Command line related.                                                     */
+/*===========================================================================*/
+
+static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[])
+{
+  size_t n, size;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: mem\r\n");
+    return;
+  }
+  n = chHeapStatus(NULL, &size);
+  chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
+  chprintf(chp, "heap fragments   : %u\r\n", n);
+  chprintf(chp, "heap free total  : %u bytes\r\n", size);
+}
+
+static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
+{
+  static const char *states[] = {CH_STATE_NAMES};
+  thread_t *tp;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: threads\r\n");
+    return;
+  }
+  chprintf(chp, " addr     pc       stack    prio refs state         name\r\n");
+  tp = chRegFirstThread();
+  do {
+    chprintf(chp, " %-8lx %-8lx %-8lx %4lu %4lu %-12s  %-10s\r\n",
+      (uint32_t)tp, (uint32_t)tp->p_ctx.r13->lr, (uint32_t)tp->p_ctx.r13,
+      (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+      states[tp->p_state],
+      tp->p_name);
+    tp = chRegNextThread(tp);
+  } while (tp != NULL);
+}
+
+static const ShellCommand commands[] = {
+  {"mem", cmd_mem},
+  {"threads", cmd_threads},
+  {NULL, NULL}
+};
+
+static const ShellConfig shellConfig = {
+  (BaseSequentialStream *)serialDriver,
+  commands
+};
+
+static thread_t *shelltp = NULL;
+static THD_WORKING_AREA(waShellThread, 768);
+
+/*===========================================================================*/
+/* Main and generic code.                                                    */
+/*===========================================================================*/
+
+
+/*
+* LEDs blinker thread, times are in milliseconds.
+*/
+static THD_WORKING_AREA(waBlinkerThread, 128);
+static THD_FUNCTION(blinker_thread, arg) {
+
+  (void)arg;
+  chRegSetThreadName("blinker");
+  while (TRUE) {
+    palSetPad(GPIOB, GPIOA_RGB_RED);
+    chThdSleepMilliseconds(250);
+    palClearPad(GPIOB, GPIOA_RGB_RED);
+    chThdSleepMilliseconds(250);
+  }
+}
+
+/*
+* Application entry point.
+*/
 int main(void) {
   /*
    * System initializations.
@@ -27,31 +110,36 @@ int main(void) {
    * - Kernel initialization, the main() function becomes a thread and the
    *   RTOS is active.
    */
+  halInit();
+  chSysInit();
 
-	halInit();
-	chSysInit();
+  /*
+   * Activates the serial driver 1 and SDC driver 1 using default
+   * configuration.
+   */
+  sdStart(serialDriver, NULL);
+  //sdcStart(&SDCD1, NULL);
 
-	// e-ink related
-	gfxInit();
-	gdispSetPowerMode(powerOn);
-	// palSetPad(GPIOC,GPIOC_EINK_RL);
-	// palSetPad(GPIOC,GPIOC_EINK_SHR);
-	// palSetPad(GPIOC,GPIOC_EINK_SPH);
-	display_init();
+  /*
+   * Shell manager initialization.
+   */
+  shellInit();
 
-	/*
-		* Activates the serial driver 3 using the driver default configuration.
-		*/
-	sdStart(&SD3, NULL);
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waBlinkerThread, sizeof(waBlinkerThread), NORMALPRIO, blinker_thread, NULL);
 
-	palClearPad(GPIOA,GPIOA_RGB_RED);
-
-	while (true) {
-		chSysLock();
-		palWritePad(GPIOC, GPIOC_EINK_CKV, TRUE);
-		chSysPolledDelayX(US2ST(5));
-		palWritePad(GPIOC, GPIOC_EINK_CKV, FALSE);
-		chSysPolledDelayX(US2ST(5));
-		chSysUnlock();	
-	}
+  /*
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and listen for events.
+   */
+  while (TRUE) {
+    if (!shelltp)
+      shelltp = shellCreateStatic(&shellConfig, waShellThread, sizeof(waShellThread), NORMALPRIO);
+    else if (chThdTerminatedX(shelltp)) {
+      chThdRelease(shelltp);    /* Recovers memory of the previous shell.   */
+      shelltp = NULL;           /* Triggers spawning of a new shell.        */
+    }
+  }
 }
